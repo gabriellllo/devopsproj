03c92a3031a9f6db2e78cd9d300253217d73c876
gabriellllo <gabriellllo@yahoo.com>
2020-05-16 14:45:07 +0200

    run pod on kubernetes and made prediction

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=gabriellllo/demoudacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+# docker pull $dockerpath:v1
+kubectl run --image=$dockerpath:v1  demoudacity --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/demoudacity 8000:80
